1ae373853acc28ccfcbace8bc59cb7408897b1e0
Pedro Bonifacio <pedro.bonifacio@effortech.com.br>
2019-05-16 14:06:18 -0300

    Commit inicial.

---
+++ build/esp-mqtt-master/component_project_vars.mk
@@ -0,0 +1,8 @@
+# Automatically generated build file. Do not edit.
+COMPONENT_INCLUDES += $(IDF_PATH)/examples/esp-mqtt-master/include
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/esp-mqtt-master -lesp-mqtt-master
+COMPONENT_LINKER_DEPS += 
+COMPONENT_SUBMODULES += 
+COMPONENT_LIBRARIES += esp-mqtt-master
+COMPONENT_LDFRAGMENTS += 
+component-esp-mqtt-master-build: 
